c42d36094ce28e03d776dec94e16f965e4b06c22
Markus Gärtner <markus.gaertner@it-agile.de>
2011-03-29 16:32:28 +0200

    took out the explicit check for the red and yellow state

--- src/org/trafficlights/domain/LightStateEditor.java
+++ src/org/trafficlights/domain/LightStateEditor.java
@@ -5,10 +5,6 @@ import java.beans.PropertyEditorSupport;
 public class LightStateEditor extends PropertyEditorSupport {
 
 	public void setAsText(String state) {
-		if ("red, yellow".equals(state)) {
-			setValue(LightState.RED_YELLOW);
-			return;
-		}
 		setValue(LightState.GREEN);
 
 		for (LightState lightState : LightState.values()) {

5f541d0e2d7ebd0987c497e27671345bb3e2d826
Markus Gärtner <markus.gaertner@it-agile.de>
2011-03-29 16:33:16 +0200

    took out the explicit setting of the green state

--- src/org/trafficlights/domain/LightStateEditor.java
+++ src/org/trafficlights/domain/LightStateEditor.java
@@ -5,8 +5,6 @@ import java.beans.PropertyEditorSupport;
 public class LightStateEditor extends PropertyEditorSupport {
 
 	public void setAsText(String state) {
-		setValue(LightState.GREEN);
-
 		for (LightState lightState : LightState.values()) {
 			if (lightState.description.equals(state)) {
 				setValue(lightState);

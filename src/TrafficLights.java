5c1101f627e39a23cc3456e60b4b7b78c622ffb7
Markus Gärtner <markus.gaertner@it-agile.de>
2011-03-29 15:23:37 +0200

    added the second test for light states and made it pass

--- src/TrafficLights.java
+++ src/TrafficLights.java
@@ -1,12 +1,15 @@
 
 public class TrafficLights {
 
+	private String state;
+	
 	public void setPreviousState(String state) {
-		
+		this.state = state;
 	}
 	
 	public String nextState() {
-		return "red, yellow";
+		if ("red".equals(state)) return "red, yellow";
+		return "green";
 	}
 	
 }

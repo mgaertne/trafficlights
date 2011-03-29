fe1aa6e5ddc1444d9671d60a7b9a4dff4a26bba1
Markus Gärtner <markus.gaertner@it-agile.de>
2011-03-29 16:23:32 +0200

    turned tests for LightStateEditor into parameterized tests and made the second test pass

--- src/org/trafficlights/domain/LightStateEditor.java
+++ src/org/trafficlights/domain/LightStateEditor.java
@@ -5,6 +5,10 @@ import java.beans.PropertyEditorSupport;
 public class LightStateEditor extends PropertyEditorSupport {
 
 	public void setAsText(String state) {
-		setValue(LightState.RED);
+		if ("red".equals(state)) {
+			setValue(LightState.RED);
+			return;
+		}
+		setValue(LightState.RED_YELLOW);
 	}
 }
--- src/org/trafficlights/domain/LightStateEditorTest.java
+++ src/org/trafficlights/domain/LightStateEditorTest.java
@@ -1,17 +1,41 @@
 package org.trafficlights.domain;
 
+import static java.util.Arrays.*;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import static org.trafficlights.domain.LightState.*;
 
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class LightStateEditorTest {
 
+	@Parameters
+	public static List<Object[]> data() {
+		return asList(new Object[][] {
+				{ "red", RED },
+				{ "red, yellow", RED_YELLOW }
+		});
+	}
+	
 	private LightStateEditor editor = new LightStateEditor();
+	private String stateName;
+	private LightState state;
+	
+	public LightStateEditorTest(String stateName, LightState state) {
+		this.stateName = stateName;
+		this.state = state;
+	}
 	
 	@Test
 	public void testname() {
-		editor.setAsText("red");
-		assertEquals(LightState.RED, editor.getValue());
+		editor.setAsText(stateName);
+		assertEquals(state, editor.getValue());
 		
 	}
 }
